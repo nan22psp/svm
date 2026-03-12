2cb2274cb87ce1795e4cde8a2deb5093083696f5
nan22psp <pyaesone35psp@gmail.com>
2026-03-12 12:50:48 +0630

    Update misc.py

--- maythusharmusic/misc.py
+++ maythusharmusic/misc.py
@@ -44,20 +44,20 @@ def dbb():
 
 async def sudo():
     global SUDOERS
-    SUDOERS.add(config.OWNER_ID)
-    sudoersdb = mongodb.sudoers
-    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
-    sudoers = [] if not sudoers else sudoers["sudoers"]
-    if config.OWNER_ID not in sudoers:
-        sudoers.append(config.OWNER_ID)
-        await sudoersdb.update_one(
-            {"sudo": "sudo"},
-            {"$set": {"sudoers": sudoers}},
-            upsert=True,
-        )
-    if sudoers:
-        for user_id in sudoers:
-            SUDOERS.add(user_id)
+    #SUDOERS.add(config.OWNER_ID)
+    #sudoersdb = mongodb.sudoers
+    #sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    #sudoers = [] if not sudoers else sudoers["sudoers"]
+    #if config.OWNER_ID not in sudoers:
+        #sudoers.append(config.OWNER_ID)
+        #await sudoersdb.update_one(
+            #{"sudo": "sudo"},
+            #{"$set": {"sudoers": sudoers}},
+            #upsert=True,
+       # )
+   # if sudoers:
+       # for user_id in sudoers:
+           # SUDOERS.add(user_id)
     LOGGER(__name__).info(f"Sudo users loaded...")
 
 
